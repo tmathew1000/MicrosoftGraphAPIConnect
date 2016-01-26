5b08b2688ac682572b4cf888a013f07bc5003fc5
tmathew1000 <tmathew1000@hotmail.com>
2016-01-25 21:17:13 -0600

    Working Version 1.0

---
+++ GraphAPIConnect/Controllers/CalendarController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Routing;
+using GraphApiConnect.Helpers;
+using GraphApiConnect.Models;
+
+namespace GraphApiConnect.Controllers
+{
+    public class CalendarController : Controller
+    {
+
+        // Take data and put into Index view.
+        public ActionResult Index(CreateEventResponse createEventResponse, EventInfo eventInfo)
+        {
+            ValidateInfo(ref eventInfo);
+
+            ViewBag.EventInfo = eventInfo;
+            ViewBag.CreateEventResponse = createEventResponse;
+
+            return View();
+        }
+
+
+        public async Task<ActionResult> CreateEventSubmit(EventInfo eventInfo)
+        {
+            // After Index method renders the View, user clicks Send Mail, which comes in here.
+            ValidateInfo(ref eventInfo);
+
+            // Create Event using the Microsoft Graph API.
+            var createEventResult = await UnifiedApiHelper.CreateEventAsync(
+                (string)Session[SessionKeys.Login.AccessToken],
+                GenerateEvent(eventInfo.StartTime, eventInfo.EndTime, eventInfo.TimeZone, eventInfo.Subject, eventInfo.Body));
+
+            // Reuse the Index view for events (created, not created, failed) .
+            // Redirect to tell the browser to call the app back via the Index method.
+            return RedirectToAction(nameof(Index), new RouteValueDictionary(new Dictionary<string, object>{
+                { "Status", createEventResult.Status },
+                { "StatusMessage", createEventResult.StatusMessage },
+                { "StartTime", eventInfo.StartTime },
+                { "EndTime", eventInfo.EndTime },
+                { "Subject", eventInfo.Subject },
+                { "TimeZone", eventInfo.TimeZone },
+                { "Body", eventInfo.Body },
+            }));
+        }
+
+        // Use the login user name or recipient email address if no user name.
+        void ValidateInfo(ref EventInfo eventInfo)
+        {
+            var currentUser = (UserInfo)Session[SessionKeys.Login.UserInfo];
+
+            if (eventInfo == null || string.IsNullOrEmpty(eventInfo.StartTime.ToString()) || string.IsNullOrEmpty(eventInfo.EndTime.ToString()) || string.IsNullOrEmpty(eventInfo.Subject))
+            {
+                eventInfo.StartTime = DateTime.Now;
+                eventInfo.EndTime = DateTime.Now.AddHours(1);
+                eventInfo.TimeZone = "Central Standard Time";
+                eventInfo.Subject = Settings.EventSubject;
+                eventInfo.Body = Settings.EventBody;
+            }
+        }
+
+        // Create email with predefine body and subject.
+        CreateEventRequest GenerateEvent(System.DateTime eventstart, System.DateTime eventend, string timezone, string subject, string body)
+        {
+            return CreateEventObject(
+
+                eventstart: eventstart,
+                eventend: eventend,
+                timezone: timezone,
+                subject: subject,
+                body: body
+            );
+        }
+
+        // Create email object in the required request format/data contract.
+        private CreateEventRequest CreateEventObject(System.DateTime eventstart, System.DateTime eventend, string timezone, string subject, string body)
+        {
+
+            return new CreateEventRequest
+            {
+
+                Event = new Event
+                {
+                    Subject = subject,
+                    Start = new DateTimeTimeZone { DateTime = eventstart.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fffK"), TimeZone = timezone },
+                    End = new DateTimeTimeZone { DateTime = eventend.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fffK"), TimeZone = timezone },
+                    //Start = new DateTimeTimeZone { DateTime = "2016-01-22T19:00:00", TimeZone = "Central Standard Time" },
+                    //End = new DateTimeTimeZone { DateTime = "2016-01-22T19:30:00", TimeZone = "Central Standard Time" },
+                    Body = new EventBody { ContentType = "Text", Content = body },
+                    ShowAs = "Free",
+                    IsAllDay = false,
+                    IsReminderOn = false
+                }
+
+            };
+        }
+    }
+}
---
+++ GraphAPIConnect/Models/UnifiedApiCalendarModels.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GraphApiConnect.Models
+{
+    public enum CreateEventStatusEnum
+    {
+        NotCreated,
+        Created,
+        Failed
+    }
+    // Data / schema contracts between this app and the Office 365 unified API server.
+    public class CreateEventResponse
+    {
+        public CreateEventStatusEnum Status { get; set; }
+        public string StatusMessage { get; set; }
+    }
+
+    public class CreateEventRequest
+    {
+        public Event Event { get; set; }
+
+    }
+    public class Event
+    {
+        public string Id { get; set; }
+        public string Subject { get; set; }
+        public DateTimeTimeZone Start { get; set; }
+        public DateTimeTimeZone End { get; set; }
+        public string ShowAs { get; set; }
+        public bool IsAllDay { get; set; }
+        public EventBody Body { get; set; }
+        public bool IsReminderOn { get; set; }
+
+        //public string bodyPreview { get; set; }
+        //public string changeKey { get; set; }
+        //public string iCalUId { get; set; }
+        //public string importance { get; set; }
+        //public bool hasAttachments { get; set; }
+
+        //public List<attendee> attendees { get; set; }
+        //public itemBody body { get; set; }
+        //public string bodyPreview { get; set; }
+        //public List<string> categories { get; set; }
+        //public string changeKey { get; set; }
+        //public string createdDateTime { get; set; }
+
+        //"attendees": odata.type": "microsoft.graph.attendee"}],
+
+        //"categories": [[{"@"string"],
+        //"createdDateTime": "String (timestamp)",
+        //"hasAttachments": true,
+        //"isCancelled": true,
+        //"isOrganizer": true,
+        //"lastModifiedDateTime": "String (timestamp)",
+        //"location": {"@odata.type": "microsoft.graph.location"},
+        //"organizer": {"@odata.type": "microsoft.graph.recipient"},
+        //"originalEndTimeZone": "string",
+        //"originalStart": "String (timestamp)",
+        //"originalStartTimeZone": "string",
+        //"recurrence": {"@odata.type": "microsoft.graph.patternedRecurrence"},
+        //"reminderMinutesBeforeStart": 1024,
+        //"responseRequested": true,
+        //"responseStatus": {"@odata.type": "microsoft.graph.responseStatus"},
+        //"sensitivity": "String",
+        //"seriesMasterId": "string",
+        //"showAs": "String",
+        //"type": "String",
+        //"webLink": "string"
+    }
+    public class DateTimeTimeZone
+    {
+        public string DateTime { get; set; }
+        public string TimeZone { get; set; }
+    }
+
+    public class EventBody
+    {
+        public string ContentType { get; set; }
+        public string Content { get; set; }
+        
+    }
+    public class EventInfo
+    {
+        public System.DateTime StartTime { get; set; }
+        public System.DateTime EndTime { get; set; }
+        public String TimeZone { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
